acf28609294dcad13eef5db7575afc976afc52e3
정재현 <chwogus0303@naver.com>
2024-05-31 19:17:43 +0900

    add(HelloIntegrationTest) : Hello 통합 테스트

---
+++ src/test/java/com/study/testcode/HelloIntegrationTest.java
@@ -0,0 +1,115 @@
+package com.study.testcode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.testcode.dto.RequestDto;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureMockMvc
+public class HelloIntegrationTest implements HelloTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private HelloRepository helloRepository;
+
+    private static final String BASE_URL = "/hello";
+
+    @Test
+    @DisplayName("Hello 생성 통합 테스트")
+    @Transactional
+    void createHello() throws Exception{
+        // given
+        RequestDto requestDto = RequestDto.builder()
+                .message(TEST_USER_MESSAGE)
+                .build();
+
+        String param = objectMapper.writeValueAsString(requestDto);
+
+        // when
+        ResultActions result = mockMvc.perform(post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(param))
+                .andDo(print());
+
+        // then
+        result.andExpect(status().isCreated());
+    }
+
+    @Nested
+    @DisplayName("Hello 조회 통합 테스트")
+    class getEntity{
+        @BeforeEach
+        void setup() {
+            helloRepository.save(HELLO);
+        }
+
+        @Test
+        @DisplayName("Hello 조회 통합 테스트")
+        @Transactional
+        void getHello() throws Exception{
+            // given
+            Long id = HELLO.getId();
+
+            // when
+            ResultActions result = mockMvc.perform(get(BASE_URL + "/{id}", id)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print());
+
+            // then
+            result.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.message").value("Hello Test"));
+        }
+    }
+
+    @Nested
+    @DisplayName("Hello 수정 통합 테스트")
+    class setEntity{
+        Hello savedHello;
+
+        @BeforeEach
+        void setup() {
+            savedHello = helloRepository.save(HELLO);
+        }
+
+        @Test
+        @DisplayName("Hello 수정 통합 테스트")
+        @Transactional
+        void setHello() throws Exception{
+            // given
+            Long id = HELLO.getId();
+
+            RequestDto requestDto = RequestDto.builder()
+                    .message(UPDATE_TEST_USER_MESSAGE)
+                    .build();
+            String param = objectMapper.writeValueAsString(requestDto);
+
+            // when
+            ResultActions result = mockMvc.perform(put(BASE_URL + "/{id}", id)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(param))
+                    .andDo(print());
+
+            // then
+            result.andExpect(status().isOk());
+            assertThat(savedHello.getMessage()).isEqualTo(UPDATE_TEST_USER_MESSAGE);
+        }
+    }
+}
